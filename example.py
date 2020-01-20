d5d3623cb017ebd7ac9782ec9b4d79f7efeb3e46
Nima Johari <johari@ucdavis.edu>
2020-01-20 15:41:51 -0800

    Add comments

--- example.py
+++ example.py
@@ -3,14 +3,19 @@ import struct
 
 lib = cdll.LoadLibrary('./libgunrock.so')
 
+# Example matrix in CSR format
+
 row_list = [0, 3, 6, 11, 15, 19, 23, 26]
 col_list = [1, 2, 3, 0, 2, 4, 0, 1, 3, 4, 5, 0, 2, 5, 6, 1, 2, 5, 6, 2, 3, 4, 6, 3, 4, 5]
+
+# We need to calculate number of nodes/edges of the base graph so we can pass to C API
 
 nodes = len(row_list) - 1
 edges = len(col_list)
 
 node = pointer((c_int * nodes)())
 
+# Likewise, calculate number of nodes/edges of the query graph, then pass to C
 
 qrow_list = row_list[:]
 qcol_list = col_list[:]
@@ -24,6 +29,8 @@ col = pointer((c_int * len(col_list))(*col_list))
 qrow = pointer((c_int * len(qrow_list))(*qrow_list))
 qcol = pointer((c_int * len(qcol_list))(*qcol_list))
 
+# This is how you might create a NodeConditionT in C or C++:
+
 # NodeConditionT node_eql_to_davis(VertexId nodeId) {
 #   NodeConditionT res;
 #   res.node = nodeId;
@@ -33,16 +40,18 @@ qcol = pointer((c_int * len(qcol_list))(*qcol_list))
 # }
 
 def NodeConditionT(node, cond, operand):
+    # This is how NodeConditionT is represented as a struct in C
+
     # struct NodeConditionT {
     #   VertexId node;  // VertexId in the query graph
     #   StrConditionT cond;
-    #   const char *operand;
+    #   const char *operand; //In fact, it must be `char operand[32];` otherwise you get SEGFAULT
     # };
 
+    # First element of the struct is a 32-bit integer
+    # Second element of the struct is an enum
+    # Third element is an array of char with maximum 32 elements.
     return struct.pack("i i 32s", node, cond, operand)
-    # return struct.pack("ii", node, cond)
-    # return struct.pack("iic", node, cond, "abc")
-    # return struct.pack("i", node)
 
 def node_eql_to_davis(nodeId):
     # return struct.pack(nodeId, 1, "davis")
@@ -69,20 +78,37 @@ def edgeLabel(src, dest, label):
 # };
 
 def edge_gt_n(src, dest, n):
-    return struct.pack("iiii", src, dest, 3, n)
+    return struct.pack("iiii", src, dest, 3, n) # edge between src and dst > n. enum for ">" is 3.
 
+# Attach node labels in the base graph
+# The order of the array corresponds to `node_id`s in Gunrock, ordered from 0..(n-1)
 base_nodeLabels = ["davis", "berkeley"]
 c_base_nodeLabels = (c_char_p * len(base_nodeLabels))()
 c_base_nodeLabels[:] = base_nodeLabels
 
+# Declare conditions on the edges of the base graph
+
+# for the query graph, set the following conditions
+#     * on node with id 42,
+#     * on node with id 45
 query_node_conditions = [node_eql_to_davis(42), node_neq_to_berkeley(45)]
+
+# pack an array of structs into one binary chunk
 c_query_node_conditions = "".join(query_node_conditions)
 
+# Labels on edges in the base graph:
+
+# Base graph may have integer as node label
 base_edgeLabels = [edgeLabel(0, 1, 30), edgeLabel(3, 4, 45)]
+# pack an array of structs into one binary chunk
 c_base_edgeLabels = "".join(base_edgeLabels)
 
 query_edgeConditions = [edge_gt_n(0, 1, 42)]
+# pack an array of structs into one binary chunk
 c_query_edgeConditions = "".join(query_edgeConditions)
+
+# Call sm_cpp defined in
+# https://github.com/johari/gunrock-c-python-sm/blob/eaf3cebe50f41f4345182958cca63705793e8d07/lambda.cpp#L180-L220
 
 elapsed = lib.sm_cpp(nodes, edges, row, col, qnodes, qedges, qrow, qcol, 1, c_base_nodeLabels, 
                     c_query_node_conditions, c_base_edgeLabels, c_query_edgeConditions, node)
